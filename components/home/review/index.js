da133b4f7e9ae3b9be5f32380871eb79da705da6
tkings <nwachukwu.kingsley01@yahoo.com>
2021-02-06 02:21:18 +0100

    fixed bug

--- components/home/review/index.js
+++ components/home/review/index.js
@@ -62,7 +62,7 @@ class Bitcoin extends Component {
                 filterUnits="userSpaceOnUse"
                 colorInterpolationFilters="sRGB"
               >
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
                 <feBlend
                   mode="normal"
                   in="SourceGraphic"
@@ -83,7 +83,7 @@ class Bitcoin extends Component {
                 filterUnits="userSpaceOnUse"
                 colorInterpolationFilters="sRGB"
               >
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
                 <feBlend
                   mode="normal"
                   in="SourceGraphic"
--- pages/about.js
+++ pages/about.js
@@ -53,8 +53,8 @@ class About extends Component {
             >
               <path
                 opacity="0.1"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M0 -6C12.8066 172.315 161.527 313 343.105 313C524.684 313 673.404 172.315 686.211 -6H598.905C586.336 124.206 476.608 226 343.105 226C209.603 226 99.8752 124.206 87.3059 -6H0Z"
                 fill="white"
               />
@@ -69,16 +69,16 @@ class About extends Component {
             >
               <g opacity="0.1" filter="url(#filter0_f)">
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M221.017 17C144.986 22.4606 84.9998 85.8726 84.9998 163.295C84.9998 240.717 144.986 304.129 221.017 309.59L221.017 272.364C165.499 267.005 122.095 220.219 122.095 163.295C122.095 106.371 165.499 59.5853 221.017 54.2259L221.017 17Z"
                   fill="white"
                 />
               </g>
               <g opacity="0.1" filter="url(#filter1_f)">
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M238.016 15.595C107.206 24.9899 4.00006 134.09 4.00005 267.295C4.00004 400.5 107.206 509.6 238.016 518.995L238.016 454.948C142.498 445.727 67.8227 365.232 67.8227 267.295C67.8227 169.358 142.498 88.8629 238.016 79.642L238.016 15.595Z"
                   fill="white"
                 />
@@ -91,9 +91,9 @@ class About extends Component {
                   width="170.017"
                   height="326.59"
                   filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
+                  colorInterpolationFilters="sRGB"
                 >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
                   <feBlend
                     mode="normal"
                     in="SourceGraphic"
@@ -112,9 +112,9 @@ class About extends Component {
                   width="242.016"
                   height="511.4"
                   filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
+                  colorInterpolationFilters="sRGB"
                 >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
                   <feBlend
                     mode="normal"
                     in="SourceGraphic"
