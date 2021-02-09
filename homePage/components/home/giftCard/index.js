dabb5d552d4f31067fdb4ed6f29d98ec4aacba3c
tkings <nwachukwu.kingsley01@yahoo.com>
2021-02-09 20:00:17 +0100

    added giftcards to homepage

--- homePage/components/home/giftCard/index.js
+++ homePage/components/home/giftCard/index.js
@@ -1,23 +1,56 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { useRouter } from "next/router";
+import { GiftCardContext } from "../../../store/root";
 import style from "./Index.module.css";
 const Index = () => {
   const router = useRouter();
+  const giftCardContext = useContext(GiftCardContext);
   return (
     <div>
-      <GiftCard router={router} />
+      <GiftCard router={router} giftCardContext={giftCardContext} />
     </div>
   );
 };
 export default Index;
 class GiftCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      giftCard: 0,
+    };
+  }
+
   handleGiftCard = (giftCard) => {
+    clearInterval(this.timeOutId);
     this.setState({
       ...this.state,
       giftCard,
     });
+    this.timeOutId = setTimeout(() => {
+      this.random();
+    }, 5000);
+  };
+  random = () => {
+    const num = Math.floor(
+      Math.random() * this.props.giftCardContext.sell.length
+    );
+    this.setState({
+      ...this.state,
+      giftCard: num,
+    });
+    this.timeOutId = setTimeout(() => {
+      this.random();
+    }, 5000);
+  };
+  timeOutId = "";
+  componentDidMount = () => {
+    this.random();
+  };
+  componentWillUnmount = () => {
+    clearInterval(this.timeOutId);
   };
   render() {
+    const giftCards = this.props.giftCardContext.sell;
     return (
       <section className={style.header}>
         <div className={style.flier}>
@@ -30,70 +63,48 @@ class GiftCard extends Component {
           <img src="/images/bg3.svg" alt="background" />
         </div>
         <div className={style.header_holder}>
-          <div className={style.info_holder}>
-            <h1>Sell all your gift cards from our Listings</h1>
-            <ul>
-              <li
-                onClick={() => {
-                  this.handleGiftCard(1);
-                }}
-              >
-                <img src="/images/bullets.svg" alt="bullet" />
-                <p>Amazon</p>
-              </li>
-              <li
-                onClick={() => {
-                  this.handleGiftCard(1);
-                }}
-              >
-                <img src="/images/bullets.svg" alt="bullet" />
-                <p>Nike</p>
-              </li>
-              <li
-                onClick={() => {
-                  this.handleGiftCard(1);
-                }}
-                className={style.active}
-              >
-                <img src="/images/bullets.svg" alt="bullet" />
-                <p>Apple</p>
-              </li>
-              <li
-                onClick={() => {
-                  this.handleGiftCard(1);
-                }}
-              >
-                <img src="/images/bullets.svg" alt="bullet" />
-                <p>Zelle</p>
-              </li>
-              <li
-                onClick={() => {
-                  this.handleGiftCard(1);
-                }}
-              >
-                <img src="/images/bullets.svg" alt="bullet" />
-                <p>Triopic</p>
-              </li>
-              <li
-                onClick={() => {
-                  this.handleGiftCard(1);
-                }}
-              >
-                <img src="/images/bullets.svg" alt="bullet" />
-                <p>Lumbic</p>
-              </li>
-            </ul>
-            <div className={style.action_buttons}>
-              <a href="/contact/speak" className={style.link_btn_gold}>
-                Get Started
-              </a>
+          {giftCards.length > 0 ? (
+            <>
+              <div className={style.info_holder}>
+                <h1>Sell all your gift cards from our Listings</h1>
+                <ul id="giftCardsList">
+                  {giftCards
+                    .slice(0, giftCards.length > 5 ? 5 : giftCards.length)
+                    .map((itm, idx) => (
+                      <li
+                        key={idx}
+                        onClick={() => {
+                          this.handleGiftCard(idx);
+                        }}
+                        className={
+                          this.state.giftCard === idx ? style.active : ""
+                        }
+                      >
+                        <img src="/images/bullets.svg" alt="bullet" />
+                        <p>{itm.name}</p>
+                      </li>
+                    ))}
+                </ul>
+                <div className={style.action_buttons}>
+                  <a href="/contact/speak" className={style.link_btn_gold}>
+                    Get Started
+                  </a>
+                </div>
+              </div>
+              <div className={style.animated_payment}>
+                <div>
+                  <img
+                    src="/images/bgImage.png"
+                    alt="animated bitcoin payment"
+                  />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className={style.load}>
+              <div className={style.loader}>Loading...</div>
             </div>
-          </div>
-          <div className={style.animated_payment}>
-            <div>
-              <img src="/images/bgImage.png" alt="animated bitcoin payment" />
-            </div>
-          </div>
+          )}
         </div>
       </section>
     );
